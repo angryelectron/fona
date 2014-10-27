6104e337c0e1d2e455f82af6e8a4395a62ee50d0
abythell <abythell@ieee.org>
2014-10-27 14:38:17 -0700

    fix serialization warning

--- src/com/angryelectron/fona/FonaException.java
+++ src/com/angryelectron/fona/FonaException.java
@@ -1,11 +1,23 @@
 /**
- * FonaException
- * Copyright 2014
- */ 
-
+ * Fona / Sim800 Library for Java Copyright 2014 Andrew Bythell
+ * <abythell@ieee.org>
+ */
 package com.angryelectron.fona;
 
-public class FonaException  extends Exception {
+/**
+ * Exception thrown by Fona classes.
+ */
+public class FonaException extends Exception {
+
+    /**
+     * Stop compiler complaining about serializable classes.
+     */
+    private static final long serialVersionUID = 1;
+
+    /**
+     * Constructor.
+     * @param message Informative message about the exception.
+     */
     public FonaException(String message) {
         super(message);
     }
