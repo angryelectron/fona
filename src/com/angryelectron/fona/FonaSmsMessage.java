a2d03fb5fc7a968f434670a31b940e2433c135b2
abythell <abythell@ieee.org>
2014-10-28 16:56:51 -0700

    update javadoc

--- src/com/angryelectron/fona/FonaSmsMessage.java
+++ src/com/angryelectron/fona/FonaSmsMessage.java
@@ -2,31 +2,72 @@
  * Fona / Sim800 Library for Java Copyright 2014 Andrew Bythell
  * <abythell@ieee.org>
  */
-
 package com.angryelectron.fona;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * SMS Message.
+ * Fona SMS message object.
  */
 public class FonaSmsMessage {
+
+    /**
+     * Sender's phone number.
+     */
     public String sender;
-    public String message;
-    public Date timestamp;
-    String response;
     
     /**
-     * Internal - set timestamp from AT command response.
-     * @param timestamp
-     * @throws ParseException 
+     * Body of message.
      */
-    void setTimestamp(String timestamp) throws ParseException {
-        // 02/01/30,20:40:31+00
-        SimpleDateFormat df = new SimpleDateFormat("yy/MM/dd,HH:mm:ss+00");
-        this.timestamp = df.parse(timestamp);
-    }
+    public String message;
     
+    /**
+     * Timestamp.
+     */
+    public Date timestamp;
+    
+    /**
+     * Message Id.
+     */
+    public Integer id;
+    
+    /**
+     * Message folder / status.
+     */
+    public Folder folder;
+        
+    /**
+     * Fona SMS message folders.
+     */
+    public enum Folder {
+
+        UNREAD("REC UNREAD"),
+        READ("REC READ"),
+        UNSENT("STO UNSENT"),
+        SENT("STO SENT"),
+        ALL("ALL");
+
+        private final String s;
+        private static final Map<String, Folder> lookup = new HashMap<>();
+
+        static {
+            for (Folder f : Folder.values()) {
+                lookup.put(f.stat(), f);
+            }
+        }
+
+        Folder(String s) {
+            this.s = s;
+        }
+
+        public String stat() {
+            return s;
+        }
+
+        public static Folder get(String s) {
+            return lookup.get(s);
+        }
+    };
 }
